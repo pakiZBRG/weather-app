8b98e33dd040fc0c8e0872d53f56269d93e13aa8
unknown <pavlovicnikola511@gmail.com>
2020-06-04 08:44:15 +0200

    first comit

--- src/components/5DayInfo.js
+++ src/components/5DayInfo.js
@@ -53,17 +53,19 @@ const FiveDayInfo = () => {
       else if(desc === "overcast clouds" || desc==="few clouds") return <img src={require('../images/overcast_clouds.png')} title='overcast clouds' alt='overcast clouds'/>
       else if(desc === "clear sky") return <img src={require('../images/clear_sky.png')} title='clear sky' alt='clear sky'/>
       else if(desc === "moderate rain") return <img src={require('../images/moderate_rain.png')} title='moderate rain' alt='moderate rain'/>
+      else if(desc === "light snow") return <img src={require('../images/light_snow.png')} title='light snow' alt='light snow'/>
+      else if(desc === "snow") return <img src={require('../images/snow.png')} title='snow' alt='snow'/>
    }
 
    return (
       <div>
-         <p className='flex_center'>Weather forecast for <span className="weather-data_city">{city}</span> as of <span className='weather-data_city'>{time.slice(5, -3)}</span></p>
+         <p className='flex_center'>Weather forecast for <span className="weather-data_city">{city}</span> as of <span className='weather-data_city'>{time.slice(10, -3)}</span></p>
          <p className='flex_center'><Link to='/' className='weather-data_5day'>Go Back</Link></p>
          <div className='flex'>
             <div className='flex_part'>
                <p className='flex_time'>{time.slice(5, -9)}</p>
                <p><span className='flex_img'>{weatherD(desc)}</span></p>
-               <p><span className='flex_misura'>Temperature: </span><span className='flex_num'>{temp.toFixed(1)}</span>°C</p>
+               <p><span className='flex_misura'>Temperature: </span><span className={temp>0 ? 'flex_num' : 'flex_blue-num'}>{temp.toFixed(1)}</span>°C</p>
                <p><span className='flex_misura'>Pressure: </span><span className='flex_num'>{pres}</span>mbar</p>
                <p><span className='flex_misura'>Humidity: </span><span className='flex_num'>{hum}</span>%</p>
                <p><span className='flex_misura'>Wind Speed: </span><span className='flex_num'>{(wind*3.6).toFixed(1)}</span>km/h</p>
@@ -71,7 +73,7 @@ const FiveDayInfo = () => {
             <div className='flex_part'>
                <p className='flex_time'>{time1.slice(5, -9)}</p>
                <p><span className='flex_img'>{weatherD(desc1)}</span></p>
-               <p><span className='flex_misura'>Temperature: </span><span className='flex_num'>{temp1.toFixed(1)}</span>°C</p>
+               <p><span className='flex_misura'>Temperature: </span><span className={temp1>0 ? 'flex_num' : 'flex_blue-num'}>{temp1.toFixed(1)}</span>°C</p>
                <p><span className='flex_misura'>Pressure: </span><span className='flex_num'>{pres1}</span>mbar</p>
                <p><span className='flex_misura'>Humidity: </span><span className='flex_num'>{hum1}</span>%</p>
                <p><span className='flex_misura'>Wind Speed: </span><span className='flex_num'>{(wind1*3.6).toFixed(1)}</span>km/h</p>
@@ -79,7 +81,7 @@ const FiveDayInfo = () => {
             <div className='flex_part'>
                <p className='flex_time'>{time2.slice(5, -9)}</p>
                <p><span className='flex_img'>{weatherD(desc2)}</span></p>
-               <p><span className='flex_misura'>Temperature: </span><span className='flex_num'>{temp2.toFixed(1)}</span>°C</p>
+               <p><span className='flex_misura'>Temperature: </span><span className={temp2>0 ? 'flex_num' : 'flex_blue-num'}>{temp2.toFixed(1)}</span>°C</p>
                <p><span className='flex_misura'>Pressure: </span><span className='flex_num'>{pres2}</span>mbar</p>
                <p><span className='flex_misura'>Humidity: </span><span className='flex_num'>{hum2}</span>%</p>
                <p><span className='flex_misura'>Wind Speed: </span><span className='flex_num'>{(wind2*3.6).toFixed(1)}</span>km/h</p>
@@ -87,7 +89,7 @@ const FiveDayInfo = () => {
             <div className='flex_part'>
                <p className='flex_time'>{time3.slice(5, -9)}</p>
                <p><span className='flex_img'>{weatherD(desc3)}</span></p>
-               <p><span className='flex_misura'>Temperature: </span><span className='flex_num'>{temp3.toFixed(1)}</span>°C</p>
+               <p><span className='flex_misura'>Temperature: </span><span className={temp3>0 ? 'flex_num' : 'flex_blue-num'}>{temp3.toFixed(1)}</span>°C</p>
                <p><span className='flex_misura'>Pressure: </span><span className='flex_num'>{pres3}</span>mbar</p>
                <p><span className='flex_misura'>Humidity: </span><span className='flex_num'>{hum3}</span>%</p>
                <p><span className='flex_misura'>Wind Speed: </span><span className='flex_num'>{(wind3*3.6).toFixed(1)}</span>km/h</p>
@@ -95,7 +97,7 @@ const FiveDayInfo = () => {
             <div className='flex_part'>
                <p className='flex_time'>{time4.slice(5, -9)}</p>
                <p><span className='flex_img'>{weatherD(desc4)}</span></p>
-               <p><span className='flex_misura'>Temperature: </span><span className='flex_num'>{temp4.toFixed(1)}</span>°C</p>
+               <p><span className='flex_misura'>Temperature: </span><span className={temp4>0 ? 'flex_num' : 'flex_blue-num'}>{temp4.toFixed(1)}</span>°C</p>
                <p><span className='flex_misura'>Pressure: </span><span className='flex_num'>{pres4}</span>mbar</p>
                <p><span className='flex_misura'>Humidity: </span><span className='flex_num'>{hum4}</span>%</p>
                <p><span className='flex_misura'>Wind Speed: </span><span className='flex_num'>{(wind4*3.6).toFixed(1)}</span>km/h</p>
@@ -103,7 +105,7 @@ const FiveDayInfo = () => {
             <div className='flex_part'>
                <p className='flex_time'>{time5.slice(5, -9)}</p>
                <p><span className='flex_img'>{weatherD(desc5)}</span></p>
-               <p><span className='flex_misura'>Temperature: </span><span className='flex_num'>{temp5.toFixed(1)}</span>°C</p>
+               <p><span className='flex_misura'>Temperature: </span><span className={temp5>0 ? 'flex_num' : 'flex_blue-num'}>{temp5.toFixed(1)}</span>°C</p>
                <p><span className='flex_misura'>Pressure: </span><span className='flex_num'>{pres5}</span>mbar</p>
                <p><span className='flex_misura'>Humidity: </span><span className='flex_num'>{hum5}</span>%</p>
                <p><span className='flex_misura'>Wind Speed: </span><span className='flex_num'>{(wind5*3.6).toFixed(1)}</span>km/h</p>
